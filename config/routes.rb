5d3fcb15c6552e18481ff3a85fe5ecb97c024510
Kelsey Evans <thekelso91@gmail.com>
2024-04-22 01:16:01 -0500

    Added to root to route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  root to: redirect('products')
   resources :products, only: [:index, :show]
 end
